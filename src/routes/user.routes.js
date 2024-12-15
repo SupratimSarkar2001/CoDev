8720b242cc370201f6eb4403c14b7e7893ab5017
Supratim <supratimsarkar2001@gmail.com>
2024-12-15 11:36:17 +0530

    Add Feed API

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth.middleware");
 const { ConnectionRequest } = require("../models/ConnectionRequest.model");
+const { User } = require("../models/Users.model");
 
 const userRouter = express.Router();
 
@@ -44,6 +45,44 @@ userRouter.get("/allConnection", userAuth, async (req,res)=>{
   res.status(400).send("Failed to fetch all Connection"+ error)
  }
 })
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+    const skip = (page - 1)*limit
+
+    limit = limit>50?10:limit;
+
+    const loggedUser = req.user;
+
+    const getAllExistingConnection = await ConnectionRequest.find({
+      $or: [
+        { fromId: loggedUser._id },
+        { toId: loggedUser._id }
+      ]
+    }).select(["fromId", "toId"]);
+
+    const blockedUserFormFeed = new Set();
+
+    blockedUserFormFeed.add(loggedUser._id);
+    
+    getAllExistingConnection.forEach((ele) => {
+      blockedUserFormFeed.add(ele.fromId);
+      blockedUserFormFeed.add(ele.toId);
+    });
+
+    const fetchedFeedUser = await User.find({
+      _id: { $nin: Array.from(blockedUserFormFeed) }
+    }).skip(skip).limit(limit);
+
+    return res.send(fetchedFeedUser);
+  } catch (error) {
+    res.status(400).send("Unable to fetch User Feed: " + error.message);
+  }
+});
+
 module.exports = {
  userRouter
 }
